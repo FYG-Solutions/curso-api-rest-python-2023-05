d169f876bcb978db479a330ba5f7edd2a3aa965e
Ramses Martinez <ramses.mtz96@gmail.com>
2023-05-31 17:13:42 -0600

    Se actualiza CRUD

--- Clase 07/sites/polls/views.py
+++ Clase 07/sites/polls/views.py
@@ -15,22 +15,51 @@ def index(request):
 def questions(request):
     if request.method == "GET":
         # Serializa la lista de objectos en un Json
-        data = serializers.serialize("json", Question.objects.all())
-        return JsonResponse(data, safe=False)
-    if request.method == "POST":
-        # Convierte el body en un dict
+        questions = Question.objects.all()
+        data = serializers.serialize("json", questions)
+        parsed_data = json.loads(data)
+        return JsonResponse(parsed_data, safe=False)
+
+    elif request.method == "POST":
         body = json.loads(request.body.decode("utf-8"))
         pub_date_str = body["pub_date"]
-        pub_date = datetime.strptime(pub_date_str, "%Y-%m-%d")  # Analiza la cadena en un objeto datetime
-
+        pub_date = datetime.strptime(pub_date_str, "%Y-%m-%d")
         new_question = Question.objects.create(
             question_text=body["question_text"],
             pub_date=pub_date,
             email=body["email"]
         )
-        # Parsea el objeto de la bdd a json
         data = serializers.serialize('json', [new_question])
-        # Turn the JSON data into a dict and send as JSON response
         return JsonResponse(json.loads(data), safe=False)
-
-
+    
+@csrf_exempt
+def question_detail(request, pk):
+    if request.method == "GET":
+        try:
+            question = Question.objects.get(pk=pk)
+            data = serializers.serialize('json', [question])
+            parsed_data = json.loads(data)
+            return JsonResponse(parsed_data, safe=False)
+        except Question.DoesNotExist:
+            return JsonResponse({"message": "Question not found"}, status=404)
+    elif request.method == "PUT":
+        body = json.loads(request.body.decode("utf-8"))
+        try:
+            question = Question.objects.get(pk=pk)
+            question.question_text = body.get("question_text", question.question_text)
+            pub_date_str = body.get("pub_date", question.pub_date.strftime("%Y-%m-%d"))
+            question.pub_date = datetime.strptime(pub_date_str, "%Y-%m-%d")
+            question.email = body.get("email", question.email)
+            question.save()
+            return JsonResponse({"message": "Question updated successfully"})
+        except Question.DoesNotExist:
+            return JsonResponse({"message": "Question not found"}, status=404)
+    elif request.method == "DELETE":
+        try:
+            question = Question.objects.get(pk=pk)
+            question.delete()
+            return JsonResponse({"message": "Question deleted successfully"})
+        except Question.DoesNotExist:
+            return JsonResponse({"message": "Question not found"}, status=404)
+    else:
+        return JsonResponse({"message": "Method not allowed"}, status=405)
